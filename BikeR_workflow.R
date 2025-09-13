27ea795dff93c31e77713ec50f3f8ef3d84b9111
Scotty0802 <152211608+Scotty0802@users.noreply.github.com>
2025-09-12 20:30:47 -0600

    Add new files

--- BikeR_workflow.R
+++ BikeR_workflow.R
@@ -7,9 +7,6 @@ library(patchwork)
 
 train<- vroom("C:/Users/riley/Desktop/Fall 2025/Stat 348/data/bike-sharing-demand/train.csv")
 test <- vroom("C:/Users/riley/Desktop/Fall 2025/Stat 348/data/bike-sharing-demand/test.csv")
-View(train)
-View(test)
-
 
 train <- train |>
   #mutate(workingday = as.factor(workingday),
@@ -21,51 +18,6 @@ train <- train |>
   #                        levels = c("1","2","3","4"),
   #                        labels = c("Spring","Summer","Fall","Winter"))) |>
   select(-casual, -registered)
-
-
-#Train Plot
-##
-GSeas <- ggplot(train, aes(x = factor(season), y = count, fill = factor(weather))) +
-  geom_bar(stat = "identity", alpha = 0.85) +
-  theme_minimal() +
-  labs(
-    x = "Season",
-    y = "Total Rentals",
-    fill = "Weather Condition",
-    title = "Total Rentals by Season and Weather"
-  ) +
-  theme(axis.text.x = element_text(angle = 45, hjust = 1))
-
-GWeather <- ggplot(train, aes(x = factor(weather), y = count, fill = factor(weather))) +
-  geom_bar(stat = "identity", alpha = 0.85) +
-  theme_minimal() +
-  labs(
-    x = "Season",
-    y = "Total Rentals",
-    fill = "Weather Condition",
-    title = "Total Rentals by Season and Weather"
-  ) +
-  theme(axis.text.x = element_text(angle = 45, hjust = 1))
-
-
-GDate <- ggplot(data = train, aes(x = datetime, y = count)) + 
-  geom_smooth(se = FALSE) +
-  theme_minimal()+
-  labs(x = "Date", y = "Count of Rentals", title = "Rentals by Date")
-
-Gtemp <- ggplot(data = train) +
-  geom_smooth(aes(x = datetime, y = temp, color = "Actual Temperature"), se = FALSE) +
-  geom_smooth(aes(x = datetime, y = atemp, color = "Feels Like Temperature"), se = FALSE) +
-  scale_color_manual(
-    name = "Type of Temperature",
-    values = c("Actual Temperature" = "red", "Feels Like Temperature" = "darkblue")
-  ) +
-  theme_minimal() +
-  labs(x = "Date", y = "Degrees in Celsius", title = "Temperature by Date") +
-  theme(legend.title = element_text(face = "bold"))
-
-(Gtemp + GWeather)/(GDate + GSeas)
-
 
 ###
 my_linear_model <- linear_reg() %>% #Type of model4
